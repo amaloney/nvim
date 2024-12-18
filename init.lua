dda1f1792d0217ffe599b47707605a8b72dc3c59
Andy Maloney <rmaloney@Mac.attlocal.net>
2024-12-18 15:55:24 -0600

    allow formatting on-off

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ vim.opt.backup = false
 vim.opt.breakindent = true
 --vim.opt.clipboard = "unnamedplus"
 vim.opt.cmdheight = 1
-vim.opt.colorcolumn = "89"
+vim.opt.colorcolumn = "101"
 vim.opt.completeopt = { "menuone", "preview", "noinsert", "noselect" }
 vim.opt.conceallevel = 0
 vim.opt.cursorline = true
@@ -37,7 +37,7 @@ vim.opt.spell = true
 vim.opt.spelllang = "en_us"
 vim.opt.swapfile = false
 vim.opt.tabstop = 4
-vim.opt.textwidth = 88
+vim.opt.textwidth = 100
 vim.opt.timeoutlen = 1000
 vim.opt.undofile = true
 vim.opt.updatetime = 300
@@ -60,30 +60,30 @@ keymap("n", "<leader>dd", "<CMD>DogeGenerate<CR>", options)
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufEnter", {
-  pattern = "*",
-  command = "set fo-=c fo-=r fo-=o",
+    pattern = "*",
+    command = "set fo-=c fo-=r fo-=o",
 })
 augroup("setIndent", { clear = true })
 autocmd("Filetype", {
-  group = "setIndent",
-  pattern = {
-    "css",
-    "html",
-    "javascript",
-    "javascriptreact",
-    "json",
-    "lua",
-    "markdown",
-    "markdownreact",
-    "scss",
-    "typescript",
-    "typescriptreact",
-    "vim",
-    "xhtml",
-    "xml",
-    "yaml",
-  },
-  command = "setlocal shiftwidth=4 tabstop=4 softtabstop=4 expandtab",
+    group = "setIndent",
+    pattern = {
+        "css",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "lua",
+        "markdown",
+        "markdownreact",
+        "scss",
+        "typescript",
+        "typescriptreact",
+        "vim",
+        "xhtml",
+        "xml",
+        "yaml",
+    },
+    command = "setlocal shiftwidth=4 tabstop=4 softtabstop=4 expandtab",
 })
 
 vim.cmd([[autocmd VimEnter * Neotree toggle left]])
@@ -98,3 +98,21 @@ vim.cmd([[set nofoldenable]])
 -- vim.cmd([[let g:python3_host_prog = $CONDA_PREFIX . "/bin/python"]])
 
 require("perovskite.lazy")
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+    else
+        vim.g.disable_autoformat = true
+    end
+end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+end, {
+    desc = "Re-enable autoformat-on-save",
+})
--- lua/perovskite/plugins/coding/conform.lua
+++ lua/perovskite/plugins/coding/conform.lua
@@ -4,45 +4,62 @@
 -- Lightweight yet powerful formatter plugin for Neovim.
 -- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      "<leader>f",
-      function()
-        require("lua.perovskite.plugins.coding.conform").format({
-          async = true,
-          lsp_fallback = true,
-        })
-      end,
-      mode = "",
-      desc = "Format buffer",
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+        {
+            -- Customize or remove this keymap to your liking
+            "<leader>f",
+            function()
+                require("lua.perovskite.plugins.coding.conform").format({
+                    async = true,
+                    lsp_fallback = true,
+                })
+            end,
+            mode = "",
+            desc = "Format buffer",
+        },
     },
-  },
-  -- Everything in opts will be passed to setup()
-  opts = {
-    -- Define your formatters
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "ruff_format" },
-      javascript = { { "prettier" } },
-      javascriptreact = { { "prettier" } },
-      typescript = { { "prettier" } },
-      typescriptreact = { { "prettier" } },
+    -- Everything in opts will be passed to setup()
+    opts = {
+        -- Define your formatters
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "ruff_format" },
+            javascript = { { "prettier" } },
+            javascriptreact = { { "prettier" } },
+            typescript = { { "prettier" } },
+            typescriptreact = { { "prettier" } },
+        },
+        -- Set up format-on-save
+        format_on_save = function(bufnr)
+            -- Disable autoformat on certain filetypes
+            local ignore_filetypes = { "sql", "java" }
+            if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+                return
+            end
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+            end
+            -- Disable autoformat for files in a certain path
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            if bufname:match("/node_modules/") then
+                return
+            end
+            -- ...additional logic...
+            return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
+        -- Customize formatters
+        formatters = {
+            shfmt = {
+                prepend_args = { "-i", "2" },
+            },
+        },
     },
-    -- Set up format-on-save
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
-    -- Customize formatters
-    formatters = {
-      shfmt = {
-        prepend_args = { "-i", "2" },
-      },
-    },
-  },
-  init = function()
-    -- If you want the formatexpr, here is the place to set it
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+    init = function()
+        -- If you want the formatexpr, here is the place to set it
+        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
 }
