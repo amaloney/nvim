155a418e0b636adf19bd7e24173f7cffc3e288e7
Andy Maloney <amaloney@mailbox.org>
2025-05-02 09:57:40 -0500

    updates for csv and md viewing

---
+++ lua/perovskite/plugins/coding/markdown-preview.lua
@@ -0,0 +1,14 @@
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+-- plugins/markdown-preview.lua
+-- https://github.com/iamcco/markdown-preview.nvim
+-- Preview Markdown in your modern browser with synchronized scrolling and flexible configuration.
+-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+return {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+}
